0ca4cdc2ba3f012b32901a1333e488d36abf19b1
NeerajX-code <neerajrathore2090@gmail.com>
2025-06-23 21:53:43 +0530

    owner my product page fix

--- src/pages/owner/MyProducts.jsx
+++ src/pages/owner/MyProducts.jsx
@@ -8,9 +8,10 @@ import Loading from '../../components/Loading';
 import { IndianRupee } from 'lucide-react';
 
 const MyProducts = () => {
-    const products = useSelector(state =>state.productReducer.products);;
+    const products = useSelector(state => state.productReducer.products);;
     const dispatch = useDispatch();
     const [isLoading, setLoading] = useState(false);
+    const [category, setCategory] = useState("allCategory")
     useEffect(() => {
         setLoading(true);
         const t = Cookies.get('token');
@@ -40,29 +41,74 @@ const MyProducts = () => {
     return (
         <div className='owner-products'>
             <div className='owner-products__search'>
-                <div style={{width:"100%",position:"relative",}}>
-                <div className='searchIcon'>
-                <Search stroke='white' />
-                </div>
-                <input
-                    type="text"
-                    className="owner-products__search-input"
-                    placeholder='Search All Of Your Products'
-                />
+                <div style={{ width: "100%", position: "relative", }}>
+                    <div className='searchIcon'>
+                        <Search stroke='white' />
+                    </div>
+                    <input
+                        type="text"
+                        className="owner-products__search-input"
+                        placeholder='Search All Of Your Products'
+                    />
                 </div>
                 <Filter fill='white' size={30} className="owner-products__search-icon" />
             </div>
             <div className="owner-products__list">
                 <div className="owner-products_category">
-                    <button>All category</button>
-                    <button>Biscuits</button>
-                    <button>Chips</button>
-                    <button>Soft Drinks</button>
-                    <button>Chocolates</button>
-                    <button>Bread</button>
-                    <button>Dairy</button>
-                    <button>Snacks</button>
-                    <button>Personal Care</button>
+                    <button
+                        style={{ backgroundColor: category === "allCategory" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("allCategory")}
+                    >
+                        All category
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Biscuits" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Biscuits")}
+                    >
+                        Biscuits
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Chips" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Chips")}
+                    >
+                        Chips
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Soft Drinks" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Soft Drinks")}
+                    >
+                        Soft Drinks
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Chocolates" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Chocolates")}
+                    >
+                        Chocolates
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Bread" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Bread")}
+                    >
+                        Bread
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Dairy" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Dairy")}
+                    >
+                        Dairy
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Snacks" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Snacks")}
+                    >
+                        Snacks
+                    </button>
+                    <button
+                        style={{ backgroundColor: category === "Personal Care" ? "orange" : "transparent" }}
+                        onClick={() => setCategory("Personal Care")}
+                    >
+                        Personal Care
+                    </button>
                 </div>
                 <div className="owner-products_div">
                     {renderProducts}
